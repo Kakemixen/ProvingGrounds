04607f70a087b4980dda8f58cb8c36aa50c14adb
Kakemixen <erlingljunggren@hotmail.com>
2018-04-13 11:56:50 +0200

    L_227

--- Source/ProvingGrounds/Private/ChooseNextWaypoint.cpp
+++ Source/ProvingGrounds/Private/ChooseNextWaypoint.cpp
@@ -2,25 +2,31 @@
 
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
-#include "PatrollingGuard.h" //TODO meh del
 #include "Classes/AIController.h"
+#include "GameFramework/Pawn.h"
+#include "PatrolRoute.h"
 
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent &OwnerComp, uint8* NodeMemory)
 {
+	APawn* AIPawn = OwnerComp.GetAIOwner()->GetPawn();
+	if (!ensure(AIPawn)) { return EBTNodeResult::Failed; }
+	UPatrolRoute* PatrolRoute = AIPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+	TArray<AActor*> Waypoints = PatrolRoute->GetPatrolPoints();
+	if (Waypoints.Num() == 0) { 
+		UE_LOG(LogTemp, Warning, TEXT("Guard missing route %s"), *AIPawn->GetName());
+		return EBTNodeResult::Failed; 
+	}
 	UBlackboardComponent* BlackboardComponent = OwnerComp.GetBlackboardComponent();
 
 	int32 Index = BlackboardComponent->GetValueAsInt(IndexKey.SelectedKeyName);
-
-	APatrollingGuard* AI = Cast<APatrollingGuard>(OwnerComp.GetAIOwner()->GetPawn());
-	if (!ensure(AI) || !ensure(AI->PatrolPointsCPP.Num() > 0)) { return EBTNodeResult::Failed; }
-
-	AActor* Waypoint = AI->PatrolPointsCPP[Index];
+	AActor* Waypoint = Waypoints[Index];
 
 	BlackboardComponent->SetValueAsObject(WaypointKey.SelectedKeyName, Waypoint);
 
-	++Index %= (AI->PatrolPointsCPP.Num()); //cycle index
+	++Index %= (Waypoints.Num()); //cycle index
 	BlackboardComponent->SetValueAsInt(IndexKey.SelectedKeyName, Index);
 
 	return EBTNodeResult::Succeeded;
---
+++ Source/ProvingGrounds/Public/PatrolRoute.h
@@ -0,0 +1,35 @@
+// Nor really copyrighted by me, Erling. (but kinda)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PatrolRoute.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class PROVINGGROUNDS_API UPatrolRoute : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UPatrolRoute();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	UPROPERTY(EditInstanceOnly, Category = "Patrol Route")
+		TArray<AActor*> PatrolPoints;
+
+public:
+	TArray<AActor*> GetPatrolPoints() const;
+		
+	
+};
